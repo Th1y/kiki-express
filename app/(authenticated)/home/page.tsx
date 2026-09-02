1b638271836604e2f4512b7faa733fb4338f6341
Th1y <tiago.pinheiro.git@gmail.com>
2026-09-02 13:08:15 -0300

    chore: reorganize sidebar and home files into authenticated layout

--- app/(authenticated)/home/page.tsx
+++ app/(authenticated)/home/page.tsx
@@ -1,6 +1,6 @@
 // app/home/page.tsx
 import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]/route";
+import { authOptions } from "../../api/auth/[...nextauth]/route";
 import { redirect } from "next/navigation";
 import Sidebar from "./sidebar";
 
---
+++ app/(authenticated)/home/sidebar.tsx
@@ -0,0 +1,20 @@
+// app/home/sidebar.tsx
+"use client";
+
+import Link from "next/link";
+import LogoutButton from "./logout-button";
+
+export default function Sidebar() {
+    return (
+        <aside className="w-64 h-screen bg-gray-800 text-white flex flex-col p-4">
+            
+            <h2 className="text-xl font-bold mb-6">Menu</h2>
+            <nav className="flex flex-col gap-3">
+                <Link href={"/home"} className="hover:underline">Home</Link>
+                <Link href={"/proposal"} className="hover:underline">Proposal</Link>
+                <Link href={"/receipt"} className="hover:underline">Receipt</Link>
+            </nav>
+            <LogoutButton/>        
+        </aside>
+    );
+}
