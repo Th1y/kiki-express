21d5b91b8dc4e5a21dbc40102d78633d061be904
Th1y <tiago.pinheiro.git@gmail.com>
2026-09-03 19:35:33 -0300

    Layout(tabbar): replace emoji icons with Heroicons for consistency

--- app/(authenticated)/home/tabbar.tsx
+++ app/(authenticated)/home/tabbar.tsx
@@ -4,19 +4,22 @@
 import Link from "next/link";
 import LogoutButton from "../logout-button";
 
+// Importando ícones do Heroicons
+import { HomeIcon, DocumentTextIcon, ReceiptRefundIcon } from "@heroicons/react/24/outline";
+
 export default function Tabbar() {
   return (
     <nav className="fixed bottom-0 left-0 right-0 bg-gray-800 text-white flex justify-around items-center p-3">
       <Link href="/home" className="flex flex-col items-center">
-        <span>🏠</span>
+        <HomeIcon className="h-6 w-6" />
         <span className="text-xs">Home</span>
       </Link>
       <Link href="/proposal" className="flex flex-col items-center">
-        <span>📄</span>
+        <DocumentTextIcon className="h-6 w-6" />
         <span className="text-xs">Proposal</span>
       </Link>
       <Link href="/receipt" className="flex flex-col items-center">
-        <span>🧾</span>
+        <ReceiptRefundIcon className="h-6 w-6" />
         <span className="text-xs">Receipt</span>
       </Link>
       <LogoutButton />
